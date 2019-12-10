f66a1bfa773505501e64352c07f81ddc6fa471ec
GfsTee <georg.treitz@gmail.com>
2019-12-10 14:20:36 +0100

    text Slider Alumni added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   // Link
 } from "react-router-dom";
 import StartSeite from './Components/StartSeite/StartSeite'
+import TextSliderAlumni from './Components/TextSliderAlumni/TextSliderAlumni';
 
 
 class App extends Component {
@@ -28,7 +29,7 @@ class App extends Component {
         <Router>
           <Switch>
             {/* component="" durch component={Zielcomponent} ersetzen */}
-            <Route path="/" component={StartSeite} exact />
+            {/* <Route path="/" component={StartSeite} exact /> */}
             <Route path="/kurse" component="" />
             <Route path="/finanzierung" component="" />
 
@@ -36,7 +37,7 @@ class App extends Component {
             <Route path="/anmeldung" component="" />
             <Route path="/impressum" component="" />
             <Route path="/datenschutz" component="" />
-            
+            <TextSliderAlumni />
             <Route component="" /> {/* für Error */}
           </Switch>
         </Router>
